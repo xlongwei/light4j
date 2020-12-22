83a5fd16793aade4fc97e4f38724af37c49176a2
xlongwei <admin@xlongwei.com>
2020-12-22 15:58:49 +0800

    记录clientPort

--- src/main/java/com/xlongwei/light4j/provider/WebSocketAbstractCallback.java
+++ src/main/java/com/xlongwei/light4j/provider/WebSocketAbstractCallback.java
@@ -1,5 +1,10 @@
 package com.xlongwei.light4j.provider;
 
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+import com.xlongwei.light4j.util.StringUtil;
+
 import io.undertow.websockets.WebSocketConnectionCallback;
 import io.undertow.websockets.core.AbstractReceiveListener;
 import io.undertow.websockets.core.BufferedTextMessage;
@@ -16,6 +21,8 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public abstract class WebSocketAbstractCallback implements WebSocketConnectionCallback {
 	private String name = getClass().getSimpleName().replace("WebSocket", "").replace("Callback", "").toLowerCase();
+	private Map<String, String> serverMapClients = new ConcurrentHashMap<>();
+	private static String[] ipHeaders = {"X-Forwarded-For", "X-Real-IP"};
 
 	@Override
 	public void onConnect(WebSocketHttpExchange exchange, WebSocketChannel channel) {
@@ -23,22 +30,47 @@ public abstract class WebSocketAbstractCallback implements WebSocketConnectionCa
 			@Override
 			protected void onFullTextMessage(WebSocketChannel channel, BufferedTextMessage message) {
 				String text = message.getData();
-				log.info("receive {} {}", channel.getPeerAddress(), text);
+				log.info("receive {} <= {} {}", getServerPort(channel), getClientPort(channel), text);
 				onTextMessage(channel, text);
 			}
 		});
-		log.info("connect {} {}", name, channel.getPeerAddress());
+		String serverPort = getServerPort(channel);
+		String clientPort = getClientPort(exchange, channel);
+		log.info("connect {} {} <=> {}", name, serverPort, clientPort);
+		serverMapClients.put(serverPort, clientPort);
 		channel.resumeReceives();
 		onConnectEvent(exchange, channel);
+	}
+	
+	/** /127.0.0.1:61642 */
+	public static String getClientPort(WebSocketHttpExchange exchange, WebSocketChannel channel) {
+		String clientPort = channel.getSourceAddress().toString();
+		String clientIp = null;
+		for(String ipHeader : ipHeaders) {
+			String ipValue = exchange.getRequestHeader(ipHeader);
+			if(!StringUtil.isBlank(ipValue) && ipValue.indexOf('.')>0) {
+				int p = ipValue.indexOf(',');
+				if(p>0) {
+					ipValue = ipValue.substring(0, p);
+				}
+				if(StringUtil.isIp(ipValue) && !"127.0.0.1".equals(ipValue)) {
+					clientIp = ipValue;
+				}
+			}
+			if(clientIp != null) {
+				break;
+			}
+		}
+		return clientIp==null ? clientPort : "/"+clientIp+clientPort.substring(clientPort.indexOf(':'));
 	}
 	
 	/** 发送消息给客户端 */
 	protected void sendText(WebSocketChannel channel, String text, boolean broadcast) {
 		if(broadcast) {
-			log.info("broadcast {} {}", channel.getPeerAddress(), text);
+			log.info("broadcast {} => {} {}", getServerPort(channel), getClientPort(channel), text);
 			channel.getPeerConnections().parallelStream().forEach(c -> WebSockets.sendText(text, c, null));
 		}else {
-			log.info("send {} {}", channel.getPeerAddress(), text);
+			log.info("send {} => {} {}", getServerPort(channel), getClientPort(channel), text);
 			WebSockets.sendText(text, channel, null);
 		}
 	}
@@ -52,4 +84,14 @@ public abstract class WebSocketAbstractCallback implements WebSocketConnectionCa
 	
 	/** 处理客户端连接事件 */
 	protected void onConnectEvent(WebSocketHttpExchange exchange, WebSocketChannel channel) { }
+	
+	/** /127.0.0.1:61642 */
+	protected String getServerPort(WebSocketChannel channel) {
+		return channel.getDestinationAddress().toString();
+	}
+	
+	/** /127.0.0.1:61642 */
+	protected String getClientPort(WebSocketChannel channel) {
+		return serverMapClients.get(getServerPort(channel));
+	}
 }
