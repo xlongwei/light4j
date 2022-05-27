6e28bb78da0334720b8a6e964ffe7b4b84b0afdc
xlongwei <admin@xlongwei.com>
2022-05-27 18:41:27 +0800

    ip/region

--- src/main/java/com/xlongwei/light4j/handler/service/IpHandler.java
+++ src/main/java/com/xlongwei/light4j/handler/service/IpHandler.java
@@ -12,6 +12,7 @@ import com.xlongwei.light4j.util.HandlerUtil;
 import com.xlongwei.light4j.util.NumberUtil;
 import com.xlongwei.light4j.util.RedisConfig;
 import com.xlongwei.light4j.util.StringUtil;
+import com.xlongwei.light4j.util.ThrowingFunction;
 
 import org.lionsoul.ip2region.DataBlock;
 import org.lionsoul.ip2region.DbConfig;
@@ -24,27 +25,29 @@ import lombok.extern.slf4j.Slf4j;
 
 /**
  * ip handler
+ * 
  * @author xlongwei
  *
  */
 @Slf4j
 public class IpHandler extends AbstractHandler {
 	static DbSearcher dbSearcher = null;
+	static ThrowingFunction<String, DataBlock> dbSearch = null;
 	static boolean memorySearch = NumberUtil.parseBoolean(RedisConfig.get("ip.memorySearch"), false);
 
 	public void region(HttpServerExchange exchange) throws Exception {
 		String ip = HandlerUtil.getParam(exchange, "ip");
 		boolean addIp = false;
-		if(StringUtil.isBlank(ip)) {
+		if (StringUtil.isBlank(ip)) {
 			ip = HandlerUtil.getParam(exchange, "showapi_userIp");
-			if(StringUtil.isBlank(ip)) {
+			if (StringUtil.isBlank(ip)) {
 				ip = HandlerUtil.getIp(exchange);
 			}
 			addIp = true;
 		}
 		Map<String, String> map = searchToMap(ip);
-		if(MapUtil.isNotEmpty(map)) {
-			if(addIp) {
+		if (MapUtil.isNotEmpty(map)) {
+			if (addIp) {
 				map.put("ip", ip);
 			}
 			HandlerUtil.setResp(exchange, map);
@@ -92,18 +95,18 @@ public class IpHandler extends AbstractHandler {
 	public void config(HttpServerExchange exchange) throws Exception {
 		String memory = HandlerUtil.getParam(exchange, "memory");
 		Boolean search = "true".equals(memory) ? Boolean.TRUE : ("false".equals(memory) ? Boolean.FALSE : null);
-		if(search!=null && search.booleanValue()!=memorySearch) {
+		if (search != null && search.booleanValue() != memorySearch) {
 			dbSearcher.close();
-			reload();
 			memorySearch = search.booleanValue();
+			reload();
 		}
 		HandlerUtil.setResp(exchange, Collections.singletonMap("memory", memorySearch));
 	}
-	
+
 	private static String zeroToEmpty(String value) {
 		return "0".equals(value) ? "" : value;
 	}
-	
+
 	static {
 		reload();
 	}
@@ -112,39 +115,32 @@ public class IpHandler extends AbstractHandler {
 		try {
 			DbConfig config = new DbConfig();
 			String dbFile = ConfigUtil.DIRECTORY + "ip2region.db";
-            dbSearcher = new DbSearcher(config, dbFile);
-		}catch(Exception e) {
+			dbSearcher = new DbSearcher(config, dbFile);
+			dbSearch = memorySearch ? dbSearcher::memorySearch : dbSearcher::btreeSearch;
+		} catch (Exception e) {
 			log.warn("fail to init DbSearcher: {}", e.getMessage());
 		}
 	}
-	
+
 	public static DataBlock search(String ip) {
-		if(StringUtil.hasLength(ip)) {
-//			if("0:0:0:0:0:0:0:1".equals(ip)) {
-//				ip = "127.0.0.1";
-//			}
-			if(Util.isIpAddress(ip)) {
-				synchronized (dbSearcher) {
-					try{
-						if(memorySearch) {
-							return dbSearcher.memorySearch(ip);
-						}
-						return dbSearcher.btreeSearch(ip);
-					}catch(Exception e) {
-						log.warn("fail to search ip: {}, ex: {}", ip, e.getMessage());
-					}
+		if (Util.isIpAddress(ip)) {
+			synchronized (dbSearcher) {
+				try {
+					return dbSearch.apply(ip);
+				} catch (Exception e) {
+					log.warn("fail to search ip: {}, ex: {}", ip, e.getMessage());
 				}
 			}
 		}
 		return null;
 	}
-	
+
 	public static Map<String, String> searchToMap(String ip) {
 		DataBlock dataBlock = search(ip);
-		if(dataBlock != null) {
-			//国家，区域，省份，城市，运营商
+		if (dataBlock != null) {
+			// 国家，区域，省份，城市，运营商
 			String region = dataBlock.getRegion();
-			if(StringUtil.hasLength(region)) {
+			if (StringUtil.hasLength(region)) {
 				Map<String, String> map = new LinkedHashMap<>(8);
 				String[] split = region.split("[|]", 5);
 				int idx = 0;
