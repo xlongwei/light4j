135ad3d75a342cdee93716f21ced08dc4c3942c6
xlongwei <admin@xlongwei.com>
2022-06-05 01:17:42 +0800

    pinyin/sort

--- src/main/java/com/xlongwei/light4j/handler/service/PinyinHandler.java
+++ src/main/java/com/xlongwei/light4j/handler/service/PinyinHandler.java
@@ -5,12 +5,16 @@ import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
+import com.alibaba.fastjson.JSONArray;
+import com.google.common.collect.Maps;
 import com.networknt.utility.StringUtils;
 import com.networknt.utility.Tuple;
 import com.xlongwei.light4j.handler.ServiceHandler.AbstractHandler;
 import com.xlongwei.light4j.util.EcdictUtil;
 import com.xlongwei.light4j.util.HandlerUtil;
+import com.xlongwei.light4j.util.JsonUtil;
 import com.xlongwei.light4j.util.NumberUtil;
 import com.xlongwei.light4j.util.PinyinUtil;
 import com.xlongwei.light4j.util.StringUtil;
@@ -27,7 +31,12 @@ public class PinyinHandler extends AbstractHandler {
 
 	@Override
 	public void handleRequest(HttpServerExchange exchange) throws Exception {
-		String text = HandlerUtil.getParam(exchange, "text");
+		String path = exchange.getAttachment(AbstractHandler.PATH);
+		if("sort".equals(path)) {
+			sort(exchange);
+			return;
+		}
+		String text = HandlerUtil.getParamOrBody(exchange, "text");
 		if(StringUtils.isNotBlank(text)) {
 			int caseType = NumberUtil.parseInt(HandlerUtil.getParam(exchange, "caseType"), 0);
 			int toneType = NumberUtil.parseInt(HandlerUtil.getParam(exchange, "toneType"), 0);
@@ -66,4 +75,32 @@ public class PinyinHandler extends AbstractHandler {
 		}
 	}
 
+	private void sort(HttpServerExchange exchange) {
+		JSONArray array = getArray(exchange);
+		if(array==null || array.isEmpty()) {
+			return;
+		}
+		int caseType = NumberUtil.parseInt(HandlerUtil.getParam(exchange, "caseType"), 0);
+		int toneType = NumberUtil.parseInt(HandlerUtil.getParam(exchange, "toneType"), 2);
+		int vcharType = NumberUtil.parseInt(HandlerUtil.getParam(exchange, "vcharType"), 1);
+		Map<Object, String> pys = new HashMap<>();
+		array.forEach(o -> pys.put(o, (o!=null && o instanceof String) ? String.join(StringUtils.EMPTY, PinyinUtil.getPinyin((String)o, caseType, toneType, vcharType)) : StringUtils.EMPTY));
+		array.sort((o1,o2) -> {
+			return pys.get(o1).compareTo(pys.get(o2));
+		});
+		Map<String,Object> map = Maps.newHashMapWithExpectedSize(4);
+		map.put("array", array);
+		if(NumberUtil.parseBoolean(HandlerUtil.getParam(exchange, "pinyin"), false)) {
+			map.put("pinyin", array.stream().map(pys::get).collect(Collectors.toList()));
+		}
+		HandlerUtil.setResp(exchange, map);
+	}
+
+	private JSONArray getArray(HttpServerExchange exchange) {
+		Object array = HandlerUtil.getObject(exchange, "array", Object.class);
+		if(array != null) {
+			return JsonUtil.parseArray(array instanceof String ? (String)array : JSONArray.toJSONString(array));
+		}
+		return JsonUtil.parseArray(HandlerUtil.getBodyString(exchange));
+	}
 }
