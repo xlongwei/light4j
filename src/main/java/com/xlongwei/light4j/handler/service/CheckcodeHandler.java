30524cf3b00d8a55cc380132103c5dce218f9d95
xlongwei <admin@xlongwei.com>
2022-07-22 21:09:31 +0800

    easy captcha

--- src/main/java/com/xlongwei/light4j/handler/service/CheckcodeHandler.java
+++ src/main/java/com/xlongwei/light4j/handler/service/CheckcodeHandler.java
@@ -1,11 +1,21 @@
 package com.xlongwei.light4j.handler.service;
 
 import java.awt.image.BufferedImage;
+import java.io.ByteArrayOutputStream;
 import java.io.OutputStream;
 import java.text.MessageFormat;
 import java.util.Map;
 
+import org.apache.commons.lang3.tuple.Triple;
+
 import com.networknt.utility.Tuple;
+import com.wf.captcha.ArithmeticCaptcha;
+import com.wf.captcha.ChineseCaptcha;
+import com.wf.captcha.ChineseGifCaptcha;
+import com.wf.captcha.GifCaptcha;
+import com.wf.captcha.SpecCaptcha;
+import com.wf.captcha.base.Captcha;
+import com.wf.captcha.base.ChineseCaptchaAbstract;
 import com.xlongwei.light4j.handler.ServiceHandler.AbstractHandler;
 import com.xlongwei.light4j.util.HandlerUtil;
 import com.xlongwei.light4j.util.IdWorker;
@@ -16,11 +26,14 @@ import com.xlongwei.light4j.util.RedisCache;
 import com.xlongwei.light4j.util.RedisConfig;
 import com.xlongwei.light4j.util.StringUtil;
 
+import cn.hutool.core.util.RandomUtil;
+import cn.hutool.core.util.StrUtil;
 import io.undertow.server.HttpServerExchange;
 import lombok.extern.slf4j.Slf4j;
 
 /**
  * checkcode handler
+ * 
  * @author xlongwei
  *
  */
@@ -29,78 +42,140 @@ public class CheckcodeHandler extends AbstractHandler {
 
 	public void image(HttpServerExchange exchange) throws Exception {
 		String checkcode = HandlerUtil.getParam(exchange, "checkcode");
-		if(StringUtil.hasLength(checkcode)) {
+		if (StringUtil.hasLength(checkcode)) {
 			String image = ImageUtil.encode(ImageUtil.bytes(ImageUtil.create(checkcode)), null);
 			HandlerUtil.setResp(exchange, StringUtil.params("image", image));
-		}else {
+		} else {
 			String sid = HandlerUtil.getParam(exchange, "sid");
-			if(sid!=null && (sid=sid.trim()).length()>13) {
+			if (sid != null && (sid = sid.trim()).length() > 13) {
 				int length = NumberUtil.parseInt(HandlerUtil.getParam(exchange, "length"), 4);
 				boolean specials = NumberUtil.parseBoolean(HandlerUtil.getParam(exchange, "specials"), false);
 				int type = NumberUtil.parseInt(HandlerUtil.getParam(exchange, "type"), -2);
-				String[] special = type<-1 ? null : ImageUtil.special(type);
-				String code = special!=null&& special.length>0 ? special[0] : null;
+				String[] special = type < -1 ? null : ImageUtil.special(type);
+				String code = special != null && special.length > 0 ? special[0] : null;
 				String check = null;
-				if(code==null) {
-					check = code = ImageUtil.random(length, specials);
-				}else {
-					check = special.length>1 ? special[1] : code;
+				Captcha captcha = null;
+				if (code == null) {
+					if (type == -3) {
+						captcha = captcha(HandlerUtil.getParam(exchange, "style"));
+						captcha.setLen(length);
+						if (!(captcha instanceof ChineseCaptchaAbstract)) {
+							int font = NumberUtil.parseInt(HandlerUtil.getParam(exchange, "font"), 0);
+							font = 0 <= font && font <= 9 ? font : (font == -1 ? RandomUtil.randomInt(0, 10) : 0);
+							captcha.setFont(font);
+						}
+						check = code = captcha.text().toLowerCase();
+					} else {
+						check = code = ImageUtil.random(length, specials);
+					}
+				} else {
+					check = special.length > 1 ? special[1] : code;
 				}
 				log.info("captcha code: {}, check: {}, sid: {}", code, check, sid);
 				RedisCache.set(ImageUtil.attr, sid, check);
 				OutputStream outputStream = exchange.getOutputStream();
-				outputStream.write(ImageUtil.bytes(ImageUtil.create(code)));
+				if (captcha == null) {
+					outputStream.write(ImageUtil.bytes(ImageUtil.create(code)));
+				} else {
+					captcha.out(outputStream);
+				}
 				outputStream.close();
 			}
 		}
 	}
-	
+
+	private String[] styles = { "gif", "chinese", "arithmetic", "chinesegif", "spec" };
+
+	private Captcha captcha(String style) {
+		switch (StrUtil.trimToEmpty(style)) {
+			case "gif":
+				return new GifCaptcha();
+			case "chinese":
+				return new ChineseCaptcha();
+			case "arithmetic":
+				return new ArithmeticCaptcha();
+			case "chinesegif":
+				return new ChineseGifCaptcha();
+			case "random":
+				return captcha(styles[RandomUtil.randomInt(0, styles.length)]);
+			default:// spec
+				return new SpecCaptcha();
+		}
+	}
+
 	public void code(HttpServerExchange exchange) throws Exception {
 		int length = NumberUtil.parseInt(HandlerUtil.getParam(exchange, "length"), 4);
 		boolean specials = NumberUtil.parseBoolean(HandlerUtil.getParam(exchange, "specials"), false);
 		int type = NumberUtil.parseInt(HandlerUtil.getParam(exchange, "type"), -2);
-		Tuple<String, String> codecheck = ImageUtil.create(length, specials, type);
-		Tuple<String, BufferedImage> tuple = ImageUtil.create(codecheck.first, codecheck.second);
-		String image = ImageUtil.encode(ImageUtil.bytes(tuple.second), null);
+		Captcha captcha = type == -3 ? captcha(HandlerUtil.getParam(exchange, "style")) : null;
+		if (captcha != null) {
+			captcha.setLen(length);
+			if (!(captcha instanceof ChineseCaptchaAbstract)) {
+				int font = NumberUtil.parseInt(HandlerUtil.getParam(exchange, "font"), 0);
+				font = 0 <= font && font <= 9 ? font : (font == -1 ? RandomUtil.randomInt(0, 10) : 0);
+				captcha.setFont(font);
+			}
+		}
+		Triple<String, String, String> triple = captcha != null ? create(captcha) : null;
+		Tuple<String, String> codecheck = triple != null
+				? new Tuple<>(triple.getLeft(), triple.getLeft())
+				: ImageUtil.create(length, specials, type);
+		Tuple<String, BufferedImage> tuple = triple != null ? new Tuple<>(triple.getMiddle(), null)
+				: ImageUtil.create(codecheck.first, codecheck.second);
+		String image = triple != null ? triple.getRight() : ImageUtil.encode(ImageUtil.bytes(tuple.second), null);
 		Map<String, String> params = StringUtil.params("sid", tuple.first, "image", image);
-		if(NumberUtil.parseBoolean(HandlerUtil.getParam(exchange, "secure"), true)==false) {
+		if (NumberUtil.parseBoolean(HandlerUtil.getParam(exchange, "secure"), true) == false) {
 			params.put("code", codecheck.first);
-			if(!codecheck.first.equals(codecheck.second) && !StringUtil.isBlank(codecheck.second)) {
+			if (!codecheck.first.equals(codecheck.second) && !StringUtil.isBlank(codecheck.second)) {
 				params.put("check", codecheck.second);
 			}
 		}
 		HandlerUtil.setResp(exchange, params);
 	}
-	
+
+	private Triple<String, String, String> create(Captcha captcha) {
+		String check = captcha.text().toLowerCase();
+		String sid = String.valueOf(IdWorker.getId());
+		RedisCache.set(ImageUtil.attr, sid, check);
+		log.info("sid:{}, check:{}, code:{}", sid, check, check);
+		ByteArrayOutputStream baos = new ByteArrayOutputStream();
+		captcha.out(baos);
+		String image = ImageUtil.encode(baos.toByteArray(), null);
+		return Triple.of(check, sid, image);
+	}
+
 	public void check(HttpServerExchange exchange) throws Exception {
 		String sid = HandlerUtil.getParam(exchange, "sid");
 		String checkcode = HandlerUtil.getParam(exchange, "checkcode");
-		if(StringUtil.hasLength(sid) && StringUtil.hasLength(checkcode)) {
+		if (StringUtil.hasLength(sid) && StringUtil.hasLength(checkcode)) {
 			String check = RedisCache.get(ImageUtil.attr, sid);
 			String valid = String.valueOf(checkcode.equalsIgnoreCase(check));
 			log.info("sid:{}, expect:{}, checkcode:{}, valid:{}", sid, check, checkcode, valid);
 			HandlerUtil.setResp(exchange, StringUtil.params("valid", valid));
 		}
 	}
-	
+
 	public void email(HttpServerExchange exchange) throws Exception {
 		String toEmail = HandlerUtil.getParam(exchange, "toEmail");
-		if(StringUtil.isEmail(toEmail)) {
+		if (StringUtil.isEmail(toEmail)) {
 			String title = StringUtil.firstNotBlank(HandlerUtil.getParam(exchange, "title"), "验证码");
 			int minutes = NumberUtil.parseInt(HandlerUtil.getParam(exchange, "minutes"), 10);
 			String checkcode = HandlerUtil.getParam(exchange, "checkcode");
-			if(StringUtil.isBlank(checkcode)) {
+			if (StringUtil.isBlank(checkcode)) {
 				checkcode = ImageUtil.random(NumberUtil.parseInt(HandlerUtil.getParam(exchange, "length"), 6), false);
 			}
 			String showapiUserName = HandlerUtil.getShowapiUserName(exchange);
 			String template = "您的验证码为{0}，有效期{1}分钟，超时请重新获取。";
-			if(!StringUtil.isBlank(showapiUserName)) {
-				//Your verification code is {0}, valid for {1} minutes, please get it again after timeout.
-				template = StringUtil.firstNotBlank(RedisConfig.get("email."+showapiUserName), template);
+			if (!StringUtil.isBlank(showapiUserName)) {
+				// Your verification code is {0}, valid for {1} minutes, please get it again
+				// after timeout.
+				template = StringUtil.firstNotBlank(RedisConfig.get("email." + showapiUserName), template);
 			}
 			String content = MessageFormat.format(template, checkcode, minutes);
-			boolean send = NumberUtil.parseBoolean(RedisConfig.get("email.switch"), true) ? MailUtil.send(toEmail, title, content) : true;
-			if(send) {
+			boolean send = NumberUtil.parseBoolean(RedisConfig.get("email.switch"), true)
+					? MailUtil.send(toEmail, title, content)
+					: true;
+			if (send) {
 				String sid = String.valueOf(IdWorker.getId());
 				RedisCache.set(ImageUtil.attr, sid, checkcode);
 				log.info("email checkcode: {}, sid: {}", checkcode, sid);
